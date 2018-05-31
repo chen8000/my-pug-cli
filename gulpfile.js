a856ccc07b46e40e0dcc7b928f85030ca767744d
zhanghui.chen <zhanghui.chen@wunderman.com>
2018-05-31 23:02:43 +0800

    23点 呵呵

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,31 @@
+
+
+// require() 模块
 const gulp = require('gulp');
+const scss = require('gulp-sass');
+
+
+// 定义一个任务
+gulp.task('scss',() => {
+
+    //去拿文件
+    gulp.src('src/scss/*.scss')
+
+        //执行编译文件的方法
+        .pipe(scss())
+
+        //把文件放到指定位置
+        .pipe(gulp.dest('dist/style/'))
+});
+
+
+//当src目录下的文件发生改变时，就会执行后面中括号里default的任务
+gulp.task('watch',()=>{
+    gulp.watch('src/**',['default'])
+});
+
+//定义一个默认任务default 执行这个任务时，会执行后面中括号里的任务
+gulp.task('default',['scss']);
+
+
 
