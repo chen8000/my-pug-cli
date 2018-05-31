bb27f8e26fb00768577007c5d4eb550df141695c
zhanghui.chen <zhanghui.chen@wunderman.com>
2018-05-31 23:42:58 +0800

    配置了bs模块

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@
 // require() 模块
 const gulp = require('gulp');
 const scss = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
+//定义一个html任务
+gulp.task('html',()=>{
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist/'))
+})
 
-// 定义一个任务
-gulp.task('scss',() => {
+// 定义一个scss任务
+gulp.task('sass',() => {
 
     //去拿文件
     gulp.src('src/scss/*.scss')
@@ -18,14 +25,19 @@ gulp.task('scss',() => {
         .pipe(gulp.dest('dist/style/'))
 });
 
-
-//当src目录下的文件发生改变时，就会执行后面中括号里default的任务
-gulp.task('watch',()=>{
-    gulp.watch('src/**',['default'])
+//定义browserSync任务
+//静态服务器
+gulp.task("serve",['default'],()=>{
+    browserSync.init({
+        server:"./dist"
+    });
+    gulp.watch('src/scss/*.scss',['sass']);
+    gulp.watch('src/*.html').on('change',reload);
 });
 
+
 //定义一个默认任务default 执行这个任务时，会执行后面中括号里的任务
-gulp.task('default',['scss']);
+gulp.task('default',['sass','html']);
 
 
 
